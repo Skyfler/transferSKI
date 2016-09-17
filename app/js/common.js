84dba7f8ec08affc3491c252bc70646bebd37942
Alpscloud <kuptsov@clouds.st>
2016-09-17 21:35:14 +0300

    add popup error, popup form, adaptive for mobile. Need make success block, fixed opacity for error popup

--- app/js/common.js
+++ app/js/common.js
@@ -1,178 +1,186 @@
 var transferApp = function () {
-		// Cache document
-		var doc = document;
+	// Cache document
+	var doc = document;
 
-		// Show Logo handler
-		var showLogo = function(e) {
-			var logo = doc.getElementById('transfer_logo'),
-				topDistance = window.pageYOffset;
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~HANDLERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// Show Logo handler
+	var showLogo = function(e) {
+		var logo = doc.getElementById('transfer_logo'),
+			formWrapper = doc.getElementById('formWrapper'),
+			offsetForm = formWrapper.offsetHeight + formWrapper.offsetTop + 15,
+			topDistance = window.pageYOffset;
 
-			if(topDistance >= 400) {
-				logo.style.cssText = "opacity: 1;";
+		if(topDistance > offsetForm) {
+			logo.style.cssText = "opacity: 1;";
 
-			} else {
-				logo.style.cssText = "opacity: 0;";
-			}   
-		};
+		} else {
+			logo.style.cssText = "opacity: 0;";
+		}   
+	};
 
-		// parallax
-		window.addEventListener('scroll', function(event) {
-		  var depth, layers, movement, topDistance, translate3d;
-		  topDistance = this.pageYOffset;
-		  layers = document.querySelectorAll("[data-type='parallax']");
+	// parallax
+	window.addEventListener('scroll', function(event) {
+	  var depth, layers, movement, topDistance, translate3d;
+	  topDistance = this.pageYOffset;
+	  layers = document.querySelectorAll("[data-type='parallax']");
 
-		  for (var i = 0; i <layers.length; i++) {
-		    depth = layers[i].getAttribute('data-depth');
-		    movement = -(topDistance * depth);
+	  for (var i = 0; i <layers.length; i++) {
+	    depth = layers[i].getAttribute('data-depth');
+	    movement = -(topDistance * depth);
 
-		    translate = 'translate(0, ' + movement + 'px)';
-		    layers[i].style['-webkit-transform'] = translate;
-		    layers[i].style['-moz-transform'] = translate;
-		    layers[i].style['-ms-transform'] = translate;
-		    layers[i].style['-o-transform'] = translate;
-		    layers[i].style.transform = translate;
-		  }
-		});
+	    translate = 'translate(0, ' + movement + 'px)';
+	    layers[i].style['-webkit-transform'] = translate;
+	    layers[i].style['-moz-transform'] = translate;
+	    layers[i].style['-ms-transform'] = translate;
+	    layers[i].style['-o-transform'] = translate;
+	    layers[i].style.transform = translate;
+	  }
+	});
 
-		// window.addEventListener('scroll', function(event) {
-		//   var depth, elem, movement, topDistance, translate3d;
+	// Close/Show mobile menu handler
+	var menuCloseBtn = doc.getElementById('mobile_menu-close'),
+		menuShowBtn = doc.getElementById('mobile_menu-show');
 
-		//   topDistance = this.pageYOffset;
-		//   elem = doc.getElementById("forma");
-		//   depth = elem.getAttribute('data-depthh');
-		//   movement = -(topDistance * depth);
-		//   translate = 'translate(-50%, ' + movement + 'px)';
-		  
-		//   elem.style['-webkit-transform'] = translate;
-		//   elem.style['-moz-transform'] = translate;
-		//   elem.style['-ms-transform'] = translate;
-		//   elem.style['-o-transform'] = translate;
-		//   elem.style.transform = translate;
-		// });
+	var toggleMobileMenu = function(e) {
+		var target = e && e.target|| e.srcElement,
+			menu = doc.getElementById('mobile_menu');
+		if(target.getAttribute('data-action') === 'show') {
+			menu.style.right = "0";
+		} else if (target.getAttribute('data-action') === 'close') {
+			menu.style.right = "-100%";
+		}
+
+	};
+
+	// Hide/show search form handler
+	var searchFormShowBtn = doc.getElementById('showFormBtn'),
+		searchFormSubmitBtn = doc.getElementById('searchSubmit'),
+		searchFormMobileSubmitBtn = doc.getElementById('searchSubmitMobile'),
+		searchFormHideBtn = doc.getElementById('hideFormBtn');
+
+	var toggleSearchForm = function(e) {
+		var target =  e && e.target || e.srcElement,
+			 searchForm = doc.getElementById('overlayForm');
+
+		if (target.getAttribute('data-show') === 'close') {
+			searchForm.style.right = "0";
+			searchFormSubmitBtn.style.width = "100%";
+		} else if (target.getAttribute('data-show') === 'show') {
+			searchForm.style.right = "-100%";
+			searchFormSubmitBtn.style.width = "220%";
+		}
+	};
+
+	// Change input value handler
+	var arrowTo = doc.getElementById('changeInputsValTo'),
+		arrowFrom = doc.getElementById('changeInputsValFrom'),
+		inputFromTo = doc.getElementById('static-place'),
+		inputArriveTo = doc.getElementById('static-destination'),
+		inputFromFrom = doc.getElementById('static-place-from'),
+		inputArriveFrom = doc.getElementById('static-destination-from');
+
+	var changeInputs = function(e) {
+		var target = e && e.target || e.srcElement,
+				valFromTo =  inputFromTo.value,
+				valArriveTo = inputArriveTo.value,
+				valFromFrom = inputFromFrom.value,
+				valArriveFrom = inputArriveFrom.value;
+
+		if(target.id === 'changeInputsValTo') {
+				inputFromTo.value = null;
+				inputArriveTo.value = null;
+				inputFromTo.value = valArriveTo;
+				inputArriveTo.value = valFromTo;
+		} else if (target.id === 'changeInputsValFrom') {
+				inputFromFrom.value = null;
+				inputArriveFrom.value = null;
+				inputFromFrom.value = valArriveFrom;
+				inputArriveFrom.value = valFromFrom;
+		}
+	};
+
+	var changeArrowActive = function(e) {
+		var target = e && e.target || e.srcElement;
+		if (target.id === 'static-place' || target.id === 'static-destination') {
+			arrowTo.classList.add('search_form-arrow--active');
+
+		} else if (target.id === 'static-place-from' || target.id === 'static-destination-from'){
+			arrowFrom.classList.add('search_form-arrow--active');
+		}
+	};
+
+	// Show/Hide error popup
+	var errorPopupHideBtn = doc.getElementById('hideErrorPopupBtn'),
+		errorPopup = doc.getElementById('errorPopup');
+
+	var showErrorPopup = function(e) {
+		e.preventDefault();
+		var target = e && e.target|| e.srcElement;		
+		if(target.getAttribute('data-id') === 'error_transfer') {
+			errorPopup.style.cssText = "opacity: 1; z-index: 2";
+		}
+	};
+
+	var hideErrorPopup = function (e) {
+		var target = e && e.target|| e.srcElement;
+		if(target.id === 'hideErrorPopupBtn') {
+			errorPopup.style.cssText = 'z-index: -1; opacity: 0';
+		}
+	};
+
+	searchFormSubmitBtn.addEventListener('click', showErrorPopup);
+	searchFormMobileSubmitBtn.addEventListener('click', showErrorPopup);
+
+	// Show/Hide error form
+	var showErrorFormBtn = doc.getElementById('showErrorFormBtn'),
+		closeErrorFormBtn = doc.getElementById('closeErrorFormBtn'),
+		errorHiddenForm = doc.getElementById('errorHiddenForm');
+
+	var showErrorForm = function (e) {
+		var target = e && e.target|| e.srcElement;
+			if(target.id === 'showErrorFormBtn') {
+				errorHiddenForm.style.right = "0";
+		}
+	};
+
+	var hideErrorForm = function (e) {
+		var target = e && e.target|| e.srcElement;
+		if(target.id === 'closeErrorFormBtn') {
+			errorHiddenForm.style.right = "-100%";
+			errorPopup.style.cssText = "opacity: 0; z-index: -1";
+		}
+	};
 
 
-		// var parallax = function () {
-		// 	var topDistance = window.pageYOffset,
-		// 		forma = doc.getElementById('forma'),
-		// 		depth=0.02,
-		// 		movement = topDistance * depth;
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~EVENTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// Show logo event
+	window.addEventListener('scroll', showLogo); 
 
-		// 	while (topDistance < 1000) {
-		// 		forma.style.top = movement + 'px';
-		// 	}
-		// };
+	// Mobile menu event
+	menuCloseBtn.addEventListener('click', toggleMobileMenu);
+	menuShowBtn.addEventListener('click', toggleMobileMenu);
 
-		// window.addEventListener('scroll', parallax);
-		// Close/Show mobile menu handler
-		var closeBtn = doc.getElementById('mobile_menu-close'),
-				showBtn = doc.getElementById('mobile_menu-show');
+	// Hide/show form event
+	searchFormHideBtn.addEventListener('click', toggleSearchForm);
+	searchFormShowBtn.addEventListener('click', toggleSearchForm);
 
-		var showMenu = function(e) {
-				var target = e && e.target|| e.srcElement,
-						menu = doc.getElementById('mobile_menu');
-				if(target.getAttribute('data-action') === 'show') {
-						menu.style.right = "0";
-				}
-		};
+	// Change value inputs event
+	arrowTo.addEventListener('click', changeInputs);
+	arrowFrom.addEventListener('click', changeInputs);
 
-		var closeMenu = function(e) {
-				var target = e && e.target|| e.srcElement,
-						menu = closeBtn.parentNode;
-				if(target.getAttribute('data-action') === 'close') {
-						menu.style.right = "-100%";
-				}
-		};
+	// Change arrow bg on focus input
+	inputFromTo.addEventListener('focus', changeArrowActive);
+	inputFromFrom.addEventListener('focus', changeArrowActive);
 
-		// Hide/show search form handler
-		var showFormBtn = doc.getElementById('showForm'),
-				hideFormBtn = doc.getElementById('hideForm');
+	// Close error popup
+	errorPopupHideBtn.addEventListener('click', hideErrorPopup);
 
-		var hideShowForm = function(e) {
-			var target =  e && e.target || e.srcElement,
-				 	searchForm = doc.getElementById('overlayForm');
+	// Show/Close error form
+	showErrorFormBtn.addEventListener('click', showErrorForm);
+	closeErrorFormBtn.addEventListener('click', hideErrorForm);
 
-			if (target.getAttribute('data-show') === 'close') {
-				searchForm.style.right = "0";
-			} else if (target.getAttribute('data-show') === 'show') {
-				searchForm.style.right = "-100%";
-			}
-		};
-
-		// Change input value handler
-		var arrowTo = doc.getElementById('changeInputsValTo'),
-				arrowFrom = doc.getElementById('changeInputsValFrom'),
-				inputFromTo = doc.getElementById('inputFromTo'),
-				inputArriveTo = doc.getElementById('inputArriveTo'),
-				inputFromFrom = doc.getElementById('inputFromFrom'),
-				inputArriveFrom = doc.getElementById('inputArriveFrom');
-		
-		var changeInputs = function(e) {
-				var target = e && e.target || e.srcElement,
-						valFromTo =  inputFromTo.value,
-						valArriveTo = inputArriveTo.value,
-						valFromFrom = inputFromFrom.value,
-						valArriveFrom = inputArriveFrom.value;
-
-				if(target.id === 'changeInputsValTo') {
-						inputFromTo.value = null;
-						inputArriveTo.value = null;
-						inputFromTo.value = valArriveTo;
-						inputArriveTo.value = valFromTo;
-				} else if (target.id === 'changeInputsValFrom') {
-						inputFromFrom.value = null;
-						inputArriveFrom.value = null;
-						inputFromFrom.value = valArriveFrom;
-						inputArriveFrom.value = valFromFrom;
-				}
-		};
-
-		// Change arrow bg event
-		// var changeArrowBlur = function(e) {
-		// 	var target = e && e.target || e.srcElement;
-		// 	if (target.id === 'inputFromTo' || target.id === 'inputArriveTo') {
-		// 		arrowTo.classList.remove('search_form-arrow--active');
-
-		// 	} else if (target.id === 'inputFromFrom' || target.id === 'inputArriveFrom'){
-		// 		arrowFrom.classList.remove('search_form-arrow--active');
-		// 	}
-		// };
-
-		var changeArrowActive = function(e) {
-			var target = e && e.target || e.srcElement;
-			if (target.id === 'inputFromTo' || target.id === 'inputArriveTo') {
-				arrowTo.classList.add('search_form-arrow--active');
-
-			} else if (target.id === 'inputFromFrom' || target.id === 'inputArriveFrom'){
-				arrowFrom.classList.add('search_form-arrow--active');
-			}
-		};
-
-		// Show logo event
-		window.addEventListener('scroll', showLogo); 
-
-		// Mobile menu event
-		closeBtn.addEventListener('click', closeMenu);
-		showBtn.addEventListener('click', showMenu);
-
-		// Hide/show form event
-		hideFormBtn.addEventListener('click', hideShowForm);
-		showFormBtn.addEventListener('click', hideShowForm);
-
-		// Change value inputs event
-		arrowTo.addEventListener('click', changeInputs);
-		arrowFrom.addEventListener('click', changeInputs);
-
-		// Change arrow bg on focus input
-		inputFromTo.addEventListener('focus', changeArrowActive);
-		inputFromFrom.addEventListener('focus', changeArrowActive);
-		// inputFromTo.addEventListener('blur', changeArrowBlur);
-		// inputFromFrom.addEventListener('blur', changeArrowBlur);
-
-		
-
-		// Parallax event
-		// window.addEventListener('scroll', parallax);
-
+	// autoresize textarea
+    // $('#error_text').autosize();   
 
 };
 
--- dist/js/common.js
+++ dist/js/common.js
@@ -1,146 +1,186 @@
 var transferApp = function () {
-		// Cache document
-		var doc = document;
+	// Cache document
+	var doc = document;
 
-		// Show Logo handler
-		var showLogo = function(e) {
-			var logo = doc.getElementById('transfer_logo'),
-				formWrapper = doc.getElementById('formWrapper'),
-				offsetForm = formWrapper.offsetHeight + formWrapper.offsetTop + 15,
-				topDistance = window.pageYOffset;
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~HANDLERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// Show Logo handler
+	var showLogo = function(e) {
+		var logo = doc.getElementById('transfer_logo'),
+			formWrapper = doc.getElementById('formWrapper'),
+			offsetForm = formWrapper.offsetHeight + formWrapper.offsetTop + 15,
+			topDistance = window.pageYOffset;
 
-			if(topDistance > offsetForm) {
-				logo.style.cssText = "opacity: 1;";
+		if(topDistance > offsetForm) {
+			logo.style.cssText = "opacity: 1;";
 
-			} else {
-				logo.style.cssText = "opacity: 0;";
-			}   
-		};
+		} else {
+			logo.style.cssText = "opacity: 0;";
+		}   
+	};
 
-		// parallax
-		window.addEventListener('scroll', function(event) {
-		  var depth, layers, movement, topDistance, translate3d;
-		  topDistance = this.pageYOffset;
-		  layers = document.querySelectorAll("[data-type='parallax']");
+	// parallax
+	window.addEventListener('scroll', function(event) {
+	  var depth, layers, movement, topDistance, translate3d;
+	  topDistance = this.pageYOffset;
+	  layers = document.querySelectorAll("[data-type='parallax']");
 
-		  for (var i = 0; i <layers.length; i++) {
-		    depth = layers[i].getAttribute('data-depth');
-		    movement = -(topDistance * depth);
+	  for (var i = 0; i <layers.length; i++) {
+	    depth = layers[i].getAttribute('data-depth');
+	    movement = -(topDistance * depth);
 
-		    translate = 'translate(0, ' + movement + 'px)';
-		    layers[i].style['-webkit-transform'] = translate;
-		    layers[i].style['-moz-transform'] = translate;
-		    layers[i].style['-ms-transform'] = translate;
-		    layers[i].style['-o-transform'] = translate;
-		    layers[i].style.transform = translate;
-		  }
-		});
+	    translate = 'translate(0, ' + movement + 'px)';
+	    layers[i].style['-webkit-transform'] = translate;
+	    layers[i].style['-moz-transform'] = translate;
+	    layers[i].style['-ms-transform'] = translate;
+	    layers[i].style['-o-transform'] = translate;
+	    layers[i].style.transform = translate;
+	  }
+	});
 
-		// Close/Show mobile menu handler
-		var closeBtn = doc.getElementById('mobile_menu-close'),
-				showBtn = doc.getElementById('mobile_menu-show');
+	// Close/Show mobile menu handler
+	var menuCloseBtn = doc.getElementById('mobile_menu-close'),
+		menuShowBtn = doc.getElementById('mobile_menu-show');
 
-		var showMenu = function(e) {
-				var target = e && e.target|| e.srcElement,
-						menu = doc.getElementById('mobile_menu');
-				if(target.getAttribute('data-action') === 'show') {
-						menu.style.right = "0";
-				}
-		};
+	var toggleMobileMenu = function(e) {
+		var target = e && e.target|| e.srcElement,
+			menu = doc.getElementById('mobile_menu');
+		if(target.getAttribute('data-action') === 'show') {
+			menu.style.right = "0";
+		} else if (target.getAttribute('data-action') === 'close') {
+			menu.style.right = "-100%";
+		}
 
-		var closeMenu = function(e) {
-				var target = e && e.target|| e.srcElement,
-						menu = closeBtn.parentNode;
-				if(target.getAttribute('data-action') === 'close') {
-						menu.style.right = "-100%";
-				}
-		};
+	};
 
-		// Hide/show search form handler
-		var showFormBtn = doc.getElementById('showForm'),
-				hideFormBtn = doc.getElementById('hideForm');
+	// Hide/show search form handler
+	var searchFormShowBtn = doc.getElementById('showFormBtn'),
+		searchFormSubmitBtn = doc.getElementById('searchSubmit'),
+		searchFormMobileSubmitBtn = doc.getElementById('searchSubmitMobile'),
+		searchFormHideBtn = doc.getElementById('hideFormBtn');
 
-		var hideShowForm = function(e) {
-			var target =  e && e.target || e.srcElement,
-				 	searchForm = doc.getElementById('overlayForm');
+	var toggleSearchForm = function(e) {
+		var target =  e && e.target || e.srcElement,
+			 searchForm = doc.getElementById('overlayForm');
 
-			if (target.getAttribute('data-show') === 'close') {
-				searchForm.style.right = "0";
-			} else if (target.getAttribute('data-show') === 'show') {
-				searchForm.style.right = "-100%";
-			}
-		};
+		if (target.getAttribute('data-show') === 'close') {
+			searchForm.style.right = "0";
+			searchFormSubmitBtn.style.width = "100%";
+		} else if (target.getAttribute('data-show') === 'show') {
+			searchForm.style.right = "-100%";
+			searchFormSubmitBtn.style.width = "220%";
+		}
+	};
 
-		// Change input value handler
-		var arrowTo = doc.getElementById('changeInputsValTo'),
-				arrowFrom = doc.getElementById('changeInputsValFrom'),
-				inputFromTo = doc.getElementById('static-place'),
-				inputArriveTo = doc.getElementById('static-destination'),
-				inputFromFrom = doc.getElementById('static-place-from'),
-				inputArriveFrom = doc.getElementById('static-destination-from');
-		
-		var changeInputs = function(e) {
-				var target = e && e.target || e.srcElement,
-						valFromTo =  inputFromTo.value,
-						valArriveTo = inputArriveTo.value,
-						valFromFrom = inputFromFrom.value,
-						valArriveFrom = inputArriveFrom.value;
+	// Change input value handler
+	var arrowTo = doc.getElementById('changeInputsValTo'),
+		arrowFrom = doc.getElementById('changeInputsValFrom'),
+		inputFromTo = doc.getElementById('static-place'),
+		inputArriveTo = doc.getElementById('static-destination'),
+		inputFromFrom = doc.getElementById('static-place-from'),
+		inputArriveFrom = doc.getElementById('static-destination-from');
 
-				if(target.id === 'changeInputsValTo') {
-						inputFromTo.value = null;
-						inputArriveTo.value = null;
-						inputFromTo.value = valArriveTo;
-						inputArriveTo.value = valFromTo;
-				} else if (target.id === 'changeInputsValFrom') {
-						inputFromFrom.value = null;
-						inputArriveFrom.value = null;
-						inputFromFrom.value = valArriveFrom;
-						inputArriveFrom.value = valFromFrom;
-				}
-		};
+	var changeInputs = function(e) {
+		var target = e && e.target || e.srcElement,
+				valFromTo =  inputFromTo.value,
+				valArriveTo = inputArriveTo.value,
+				valFromFrom = inputFromFrom.value,
+				valArriveFrom = inputArriveFrom.value;
 
-		// Change arrow bg event
-		// var changeArrowBlur = function(e) {
-		// 	var target = e && e.target || e.srcElement;
-		// 	if (target.id === 'inputFromTo' || target.id === 'inputArriveTo') {
-		// 		arrowTo.classList.remove('search_form-arrow--active');
+		if(target.id === 'changeInputsValTo') {
+				inputFromTo.value = null;
+				inputArriveTo.value = null;
+				inputFromTo.value = valArriveTo;
+				inputArriveTo.value = valFromTo;
+		} else if (target.id === 'changeInputsValFrom') {
+				inputFromFrom.value = null;
+				inputArriveFrom.value = null;
+				inputFromFrom.value = valArriveFrom;
+				inputArriveFrom.value = valFromFrom;
+		}
+	};
 
-		// 	} else if (target.id === 'inputFromFrom' || target.id === 'inputArriveFrom'){
-		// 		arrowFrom.classList.remove('search_form-arrow--active');
-		// 	}
-		// };
+	var changeArrowActive = function(e) {
+		var target = e && e.target || e.srcElement;
+		if (target.id === 'static-place' || target.id === 'static-destination') {
+			arrowTo.classList.add('search_form-arrow--active');
 
-		var changeArrowActive = function(e) {
-			var target = e && e.target || e.srcElement;
-			if (target.id === 'static-place' || target.id === 'static-destination') {
-				arrowTo.classList.add('search_form-arrow--active');
+		} else if (target.id === 'static-place-from' || target.id === 'static-destination-from'){
+			arrowFrom.classList.add('search_form-arrow--active');
+		}
+	};
 
-			} else if (target.id === 'static-place-from' || target.id === 'static-destination-from'){
-				arrowFrom.classList.add('search_form-arrow--active');
-			}
-		};
+	// Show/Hide error popup
+	var errorPopupHideBtn = doc.getElementById('hideErrorPopupBtn'),
+		errorPopup = doc.getElementById('errorPopup');
 
-		// Show logo event
-		window.addEventListener('scroll', showLogo); 
+	var showErrorPopup = function(e) {
+		e.preventDefault();
+		var target = e && e.target|| e.srcElement;		
+		if(target.getAttribute('data-id') === 'error_transfer') {
+			errorPopup.style.cssText = "opacity: 1; z-index: 2";
+		}
+	};
 
-		// Mobile menu event
-		closeBtn.addEventListener('click', closeMenu);
-		showBtn.addEventListener('click', showMenu);
+	var hideErrorPopup = function (e) {
+		var target = e && e.target|| e.srcElement;
+		if(target.id === 'hideErrorPopupBtn') {
+			errorPopup.style.cssText = 'z-index: -1; opacity: 0';
+		}
+	};
 
-		// Hide/show form event
-		hideFormBtn.addEventListener('click', hideShowForm);
-		showFormBtn.addEventListener('click', hideShowForm);
+	searchFormSubmitBtn.addEventListener('click', showErrorPopup);
+	searchFormMobileSubmitBtn.addEventListener('click', showErrorPopup);
 
-		// Change value inputs event
-		arrowTo.addEventListener('click', changeInputs);
-		arrowFrom.addEventListener('click', changeInputs);
+	// Show/Hide error form
+	var showErrorFormBtn = doc.getElementById('showErrorFormBtn'),
+		closeErrorFormBtn = doc.getElementById('closeErrorFormBtn'),
+		errorHiddenForm = doc.getElementById('errorHiddenForm');
 
-		// Change arrow bg on focus input
-		inputFromTo.addEventListener('focus', changeArrowActive);
-		inputFromFrom.addEventListener('focus', changeArrowActive);
-		// inputFromTo.addEventListener('blur', changeArrowBlur);
-		// inputFromFrom.addEventListener('blur', changeArrowBlur);
+	var showErrorForm = function (e) {
+		var target = e && e.target|| e.srcElement;
+			if(target.id === 'showErrorFormBtn') {
+				errorHiddenForm.style.right = "0";
+		}
+	};
 
+	var hideErrorForm = function (e) {
+		var target = e && e.target|| e.srcElement;
+		if(target.id === 'closeErrorFormBtn') {
+			errorHiddenForm.style.right = "-100%";
+			errorPopup.style.cssText = "opacity: 0; z-index: -1";
+		}
+	};
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~EVENTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// Show logo event
+	window.addEventListener('scroll', showLogo); 
+
+	// Mobile menu event
+	menuCloseBtn.addEventListener('click', toggleMobileMenu);
+	menuShowBtn.addEventListener('click', toggleMobileMenu);
+
+	// Hide/show form event
+	searchFormHideBtn.addEventListener('click', toggleSearchForm);
+	searchFormShowBtn.addEventListener('click', toggleSearchForm);
+
+	// Change value inputs event
+	arrowTo.addEventListener('click', changeInputs);
+	arrowFrom.addEventListener('click', changeInputs);
+
+	// Change arrow bg on focus input
+	inputFromTo.addEventListener('focus', changeArrowActive);
+	inputFromFrom.addEventListener('focus', changeArrowActive);
+
+	// Close error popup
+	errorPopupHideBtn.addEventListener('click', hideErrorPopup);
+
+	// Show/Close error form
+	showErrorFormBtn.addEventListener('click', showErrorForm);
+	closeErrorFormBtn.addEventListener('click', hideErrorForm);
+
+	// autoresize textarea
+    // $('#error_text').autosize();   
 
 };
 
